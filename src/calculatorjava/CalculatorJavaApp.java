deb34b366df7be8fa1a308323d6797f48dbf34c1
Richard Allen <richard2891@gmail.com>
2019-04-30 18:50:54 -0400

    initial commit, this is version v1

---
+++ src/calculatorjava/CalculatorJavaApp.java
@@ -0,0 +1,44 @@
+/*
+ * CalculatorJavaApp.java
+ */
+
+package calculatorjava;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class CalculatorJavaApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new CalculatorJavaView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of CalculatorJavaApp
+     */
+    public static CalculatorJavaApp getApplication() {
+        return Application.getInstance(CalculatorJavaApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(CalculatorJavaApp.class, args);
+    }
+}
